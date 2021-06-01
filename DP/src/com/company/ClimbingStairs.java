aa772651ef13778cea01a935fc2eb1c2c7e3c5eb
Akash Verma <sky4032.av@gmail.com>
2021-06-01 20:43:00 +0530

    tabulation

--- DP/src/com/company/ClimbingStairs.java
+++ DP/src/com/company/ClimbingStairs.java
@@ -1,2 +1,65 @@
-package com.company;public class ClimbingStairs {
+package com.company;
+import java.util.Scanner;
+
+public class ClimbingStairs {
+    public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int cp = countpaths(n);
+        System.out.println(cp);
+    }
+
+ //   RECURSION
+//    public static int countpaths(int n){
+//        if(n == 0){
+//            return 1;
+//        }
+//         else if(n < 0){
+//            return 0;
+//        }
+//        System.out.println("Hello" +n);
+//         int nm1 = countpaths(n-1);
+//         int nm2 = countpaths(n-2);
+//         int nm3 = countpaths(n-3);
+//         int cp = nm1 +nm2 +nm3;
+//
+//         return cp;
+//    }
+//
+//    //  MEMOIZATION
+//    public static int countpaths(int n, int[] qb){
+//        if(n == 0){
+//            return 1;
+//        }
+//        else if(n < 0){
+//            return 0;
+//        }
+//        if (qb[n]> 0){
+//            return qb[n];
+//        }
+//        System.out.println("Hello" +n);
+//        int nm1 = countpaths(n-1, qb);
+//        int nm2 = countpaths(n-2, qb);
+//        int nm3 = countpaths(n-3, qb);
+//        int cp = nm1 +nm2 +nm3;
+//
+//        qb[n] = cp;
+//        return cp;
+
+    //  TABULATION
+    public static int countpaths(int n){
+        int[] dp = new int [ n + 1];
+
+        dp[0]=1;
+        for (int i =1; i <= n; i++) {
+            if (i == 1) {
+                dp[i] = dp[i - 1];
+            } else if (i == 2) {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            } else {
+                dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+            }
+        }return dp[n];
+    }
 }
--- DP/src/com/company/Main.java
+++ DP/src/com/company/Main.java
@@ -8,19 +8,32 @@ public class Main {
         // write your code here
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
-        int fibn = fib(n);
+        int fibn = fibmemoized(n, new int[n + 1]);
         System.out.println(fibn);
     }
 
-    public static int fib(int n) {
+//    public static int fib(int n) {
+//        if (n == 0 || n == 1) {
+//            return n;
+//        }
+//        int fibm1 = fib(n - 1);
+//        int fibm2 = fib(n - 2);
+//        int fib = fibm1 + fibm2;
+//
+//        return fib;
+
+    public static int fibmemoized(int n, int[] qb) {
         if (n == 0 || n == 1) {
             return n;
         }
-        int fibm1 = fib(n - 1);
-        int fibm2 = fib(n - 2);
+        if (qb[n] != 0) {
+            return qb[n];
+        }
+        int fibm1 = fibmemoized(n - 1, qb);
+        int fibm2 = fibmemoized(n - 2, qb);
         int fib = fibm1 + fibm2;
 
+        qb[n] = fib;
         return fib;
-
     }
 }
