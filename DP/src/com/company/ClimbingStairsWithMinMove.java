d190a2a2bb25780c32ca71f32c13613baa721cfc
Akash Verma <sky4032.av@gmail.com>
2021-06-05 03:22:15 +0530

    in coast path

--- DP/src/com/company/ClimbingStairsWithMinMove.java
+++ DP/src/com/company/ClimbingStairsWithMinMove.java
@@ -1,2 +1,30 @@
-package com.company;public class ClimbingStairsWithMinMove {
+package com.company;
+
+import java.util.Scanner;
+
+public class ClimbingStairsWithMinMove {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scn.nextInt();
+        }
+        Integer[] dp = new Integer[n + 1];
+        dp[n] = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            if (arr[i] > 0) {
+                int min = Integer.MAX_VALUE;
+                for (int j = 1; j <= arr[i] && i + j < dp.length; j++) {
+                    if (dp[i + j] != null) {
+                        min = Math.min(min, dp[i + j]);
+                    }
+                }
+                if (min != Integer.MAX_VALUE){
+                    dp[i] = min + 1;
+                }
+            }
+        }
+        System.out.println(dp[0]);
+    }
 }
