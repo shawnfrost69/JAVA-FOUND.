482d194845fe96499aa84e8fea1359ac6c5a60b6
Akash Verma <65110494+shawnfrost69@users.noreply.github.com>
2021-06-12 12:23:08 +0530

    Add files via upload

--- DP/src/com/company/CountBinaryString.java
+++ DP/src/com/company/CountBinaryString.java
@@ -4,19 +4,32 @@ import java.util.Scanner;
 
 public class CountBinaryString {
     public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        int n = scn.nextInt();
+    Scanner scn = new Scanner(System.in);
+     int n = scn.nextInt();
+//
+//        int[] dp0 = new int[n + 1];
+//        int[] dp1 = new int[n + 1];
+//
+//        dp0[1] =1;
+//        dp1[1] =1;
+//
+//        for (int i =2; i <= n; i++){
+//            dp1[i] = dp1[i-1] + dp0[i-1];
+//            dp0[i] = dp1[i-1];
+//        }
+//        System.out.println(dp1[n] + dp0[n]);
+//    }
 
-        int[] dp0 = new int[n + 1];
-        int[] dp1 = new int[n + 1];
-
-        dp0[1] =1;
-        dp1[1] =1;
+        int oczeros = 1;
+        int ocones = 1;
 
         for (int i =2; i <= n; i++){
-            dp1[i] = dp1[i-1] + dp0[i-1];
-            dp0[i] = dp1[i-1];
+            int nczeros = ocones;
+            int ncones = oczeros + ocones;
+
+            ocones = ncones;
+            oczeros = nczeros;
         }
-        System.out.println(dp1[n] + dp0[n]);
+        System.out.println(oczeros + ocones);
     }
 }
