bc740d9cd50890a0c9ea776e6e4535d91d8da26a
Akash Verma <65110494+shawnfrost69@users.noreply.github.com>
2021-06-30 02:49:52 +0530

    Add files via upload

---
+++ OOPS/src/com/company/ClassAndObject.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class ClassAndObject {
+    public static class Person{
+        int age;
+        String name;
+
+        void saysHi(){
+            System.out.println(name + "[" + age + "] says hi");
+        }
+    }
+    public static void main(String[] args) {
+        Person p1 =new Person();
+        p1.age = 10;
+        p1.name = "A";
+        p1.saysHi();
+
+        Person p2 =new Person();
+        p2.age = 20;
+        p2.name = "B";
+        p2.saysHi();
+
+        Person p3 =p1;
+        p3.age =30;
+        p3.saysHi();
+        p1.saysHi();
+
+    }
+}
